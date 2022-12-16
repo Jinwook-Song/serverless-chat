f6cb045122cb47759876caae6516ffe283cccd24
jinwook song <wlsdnr129@naver.com>
2022-12-16 22:46:26 +0900

    feat: durable object

--- README.md
+++ README.md
@@ -21,4 +21,64 @@ State를 유지하는 방법에는 KV database가 있는데 chat state를 저장
 
 Durable object는 javascript class를 cloudflare 메모리에 저장하고, 모든 workers가 이 메모리에 접근할 수 있다.
 
+Durable object는 worker에 의해 호출되며, worker는 user에 의해 호출된다.
+
+(user가 direct로 durable object를 호출할 수 없다)
+
 ---
+
+unique Id로 cloudflare instance를 생성하고 관리하도록
+cloudflare는 class를 기억하기 때문에 어느곳이든 동일한 state를 사용할 수 있다.
+
+wrangler.toml
+
+```toml
+[durable_objects]
+bindings = [
+    {name = 'COUNTER', class_name = 'CounterObject'}
+]
+```
+
+index.ts
+
+```tsx
+export interface Env {
+  COUNTER: DurableObjectNamespace;
+}
+
+export class CounterObject {
+  counter: number;
+  constructor() {
+    this.counter = 0;
+  }
+  async fetch(request: Request) {
+    const { pathname } = new URL(request.url);
+    switch (pathname) {
+      case '/':
+        return new Response(JSON.stringify({ counter: this.counter }));
+      case '/+':
+        this.counter++;
+        return new Response(JSON.stringify({ counter: this.counter }));
+      case '/-':
+        this.counter--;
+        return new Response(JSON.stringify({ counter: this.counter }));
+      default:
+        return handleNotFound();
+    }
+  }
+}
+
+export default {
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const id = env.COUNTER.idFromName('counter');
+    // durable object가 있다면 반환하고, 없으면 생성하고 초기화
+    const durableObject = env.COUNTER.get(id);
+    const response = await durableObject.fetch(request);
+    return response;
+  },
+};
+```
--- src/index.ts
+++ src/index.ts
@@ -9,22 +9,65 @@
  */
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
+  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+  // MY_KV_NAMESPACE: KVNamespace;
+  //
+  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+  COUNTER: DurableObjectNamespace;
+  //
+  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+  // MY_BUCKET: R2Bucket;
+}
+
+// @ts-ignore
+import home from './home.html';
+
+function handleHome() {
+  return new Response(home, {
+    headers: {
+      'Content-Type': 'text/html;chartset=utf-8',
+    },
+  });
+}
+
+function handleNotFound() {
+  return new Response(null, {
+    status: 404,
+  });
+}
+
+export class CounterObject {
+  counter: number;
+  constructor() {
+    this.counter = 0;
+  }
+  async fetch(request: Request) {
+    const { pathname } = new URL(request.url);
+    switch (pathname) {
+      case '/':
+        return new Response(JSON.stringify({ counter: this.counter }));
+      case '/+':
+        this.counter++;
+        return new Response(JSON.stringify({ counter: this.counter }));
+      case '/-':
+        this.counter--;
+        return new Response(JSON.stringify({ counter: this.counter }));
+      default:
+        return handleNotFound();
+    }
+  }
 }
 
 export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		return new Response("Hello World!");
-	},
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const id = env.COUNTER.idFromName('counter');
+    // durable object가 있다면 반환하고, 없으면 생성하고 초기화
+    const durableObject = env.COUNTER.get(id);
+    const response = await durableObject.fetch(request);
+    return response;
+  },
 };
